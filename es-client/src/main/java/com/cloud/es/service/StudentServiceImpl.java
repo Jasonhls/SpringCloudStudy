9fff3ad7748bb1feb8610c56f91b56f6b3c1f716
何立森 <lisen.he@marketingforce.com>
2024-07-10 13:39:25 +0800

    去掉seata相关的代码

--- es-client/src/main/java/com/cloud/es/service/StudentServiceImpl.java
+++ es-client/src/main/java/com/cloud/es/service/StudentServiceImpl.java
@@ -3,7 +3,6 @@ package com.cloud.es.service;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.cloud.es.mapper.StudentMapper;
 import com.cloud.es.pojo.Student;
-import io.seata.spring.annotation.GlobalTransactional;
 import org.springframework.stereotype.Service;
 
 /**
@@ -14,7 +13,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class StudentServiceImpl extends ServiceImpl<StudentMapper, Student> implements StudentService {
     @Override
-    @GlobalTransactional(rollbackFor = Exception.class)
     public void saveStudent(Student student) {
         if(student.getAge() > 200) {
             throw new RuntimeException("年龄超限了");
